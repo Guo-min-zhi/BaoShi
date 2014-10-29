03713c3b3066c38c0eba3d8783dff2f66b4db72b
Guominzhi <eric.minzhi.guo@gmail.com>
2014-10-29 17:23:36 +0800

    init

---
+++ 我的个人中心_files/console.log.js
@@ -0,0 +1,11 @@
+ 
+/*重写connsole.log,防止调试代码出错*/
+if (!window.console){
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+
+    window.console = (typeof window.loadFirebugConsole == 'function') ? window.loadFirebugConsole() : {};
+    for (var i = 0; i < names.length; ++i){
+        window.console[names[i]] = function() {};
+    }
+}
